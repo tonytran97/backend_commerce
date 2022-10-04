b90a033c871b15c50d48b1f3d3175ff35324b5b9
tonytran97 <tonytran_97@live.com>
2022-10-03 22:52:43 -0700

    fixed seed error

--- models/index.js
+++ models/index.js
@@ -8,21 +8,29 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {ProductTag},
+  through: {
+  model: ProductTag
+},
 foreignKey: 'product_id',
-})
+});
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {ProductTag},
+  through: {
+  model: ProductTag
+},
   foreignKey: 'tag_id',
-})
+});
+
 module.exports = {
   Product,
   Category,
